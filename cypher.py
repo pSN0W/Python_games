b570205a7d4ac0d7f1daf2fb202555aa7fbc9e08
Pratyaksh Singh <76648453+pSN0W@users.noreply.github.com>
2022-06-26 17:05:08 +0530

    Caesar Cypher game

---
+++ cypher.py
@@ -0,0 +1,34 @@
+import art
+from os import system
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+system('clear')
+
+def caesor(text,shift,encryption):
+    if encryption == "decode":
+        shift*=-1
+    final_text = ""
+    for chars in text:
+        if chars in alphabet:
+            k = alphabet.index(chars)
+            position = k+shift
+            final_text+=alphabet[position%26]
+        else:
+            final_text+=chars
+    print(f"Here is the {encryption}d result {final_text}")
+
+
+print(art.logo)
+
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesor(text,shift,direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
